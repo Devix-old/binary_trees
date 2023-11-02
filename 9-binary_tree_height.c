da865ea462cd1c950d2d5401fe4515a1d2cdfa63
Devix-old <mohamed.elgharef@gmail.com>
2023-11-02 12:04:52 +0100

    task 1 update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,10 +8,12 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l = 0, height_r = 0;
+	const binary_tree_t *temp;
 
 	if (!tree)
 		return (0);
-	const binary_tree_t *temp = tree;
+
+	temp = tree;
 
 	while (temp != NULL)
 	{
