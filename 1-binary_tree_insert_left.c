a0fe86075ba93f5b6ca4cfeff31f0e693c1e06b2
Devix-old <mohamed.elgharef@gmail.com>
2023-11-02 11:45:36 +0100

    task 1 update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,18 +8,22 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *left_child;
 
 	if (parent == NULL)
 		return (NULL);
-	if (new_node == NULL)
+
+	left_child = binary_tree_node(parent, value);
+
+	if (left_child == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
-	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
-	}
-	parent->left = new_node;
-	return (new_node);
+	left_child->left = parent->left;
+
+	if (left_child->left != NULL)
+		left_child->left->parent = left_child;
+
+	parent->left = left_child;
+
+	return (left_child);
 }
